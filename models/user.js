cf283419223dd820884ebbd1a5912e6e350da258
zhixuan <liu.zhixu@northeastern.edu>
2024-12-10 14:18:18 -0800

    complete register functionality

--- models/user.js
+++ models/user.js
@@ -3,8 +3,9 @@ const mongoose = require('mongoose');
 // define the schema and model for User
 const userSchema = new mongoose.Schema({
     username: {type: String, required:true },
-    email: {type: String, required:true},
-    registeredAt: {type:Date, default:Date.now}
+    email: {type: String, required:true },
+    password: {type: String, required:true },
+    registeredAt: {type:Date, default:Date.now }
 });
 
 const User = mongoose.model("User", userSchema);
--- routes/register.js
+++ routes/register.js
@@ -1,21 +1,30 @@
 const express = require('express');
+const bcrypt = require('bcrypt');
 const router = express.Router();
 const User = require('../models/user');
 
 // route to handle user registration
-router.post('/', (req, res) => {
-    const { username, email } = req.body;
-    if (!username || !email) {
-        return res.status(400).json({ success: false, message: 'Username and email are required.' });
+// use async and await to handle asynchronous operations(password hashing)
+router.post('/', async (req, res) => {
+    const { username, email, password } = req.body;
+    if (!username || !email || !password) {
+        return res.status(400).json({ success: false, message: 'Username, email, and password are required.' });
     }
-    const newUser = new User({ username, email });
-    newUser.save()
-    .then(()=>{
+
+    try{
+        // encrypt the password using bcrypt with the specified salt rounds
+        const saltRounds = 10;
+        const hashedPassword = await bcrypt.hash(password, saltRounds);
+        const newUser = new User({ 
+            username,
+            email,
+            password: hashedPassword,
+        });
+        await newUser.save();
         res.status(201).json({ success: true, message: 'User registered successfully!' });
-    })
-    .catch(err => {
-        res.status(400).json({ success: false, message: 'Error: ' + err });
-    });
+    } catch(err) {
+        res.status(500).json({ success: false, message: 'Error: ' + err });
+    }
 });
 
 module.exports = router;
